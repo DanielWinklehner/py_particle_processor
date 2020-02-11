6b6d7623fc53f31b7eb12f9173696c7132ea6aad
Daniel Winklehner <winklehn@mit.edu>
2020-02-11 11:39:17 -0500

    modified halo limits again. Need to make this a user parameter!!! -DW

--- py_particle_processor_qt/tools/BeamChar/BeamChar.py
+++ py_particle_processor_qt/tools/BeamChar/BeamChar.py
@@ -314,13 +314,15 @@ class BeamChar(AbstractTool):
             plt.rc('text', usetex=True)
             plt.rc('grid', linestyle=':')
 
+            ylim = 25.0
+
             ax1 = plt.subplot(311)
             plt.title("Halo Parameter")
             for n in range(num):
                 plt.plot(plots["plot_data{}".format(n)]["energy"], plots["plot_data{}".format(n)]["xHalo"], lw=0.8,
                          label=plots["plot_data{}".format(n)]["name"])
             ax1.get_yaxis().set_major_locator(LinearLocator(numticks=5))
-            ax1.set_ylim([0, 100.0])
+            ax1.set_ylim([0, ylim])
             plt.grid()
             plt.ylabel("Horizontal")
 
@@ -330,7 +332,7 @@ class BeamChar(AbstractTool):
                          label=plots["plot_data{}".format(n)]["name"])
             plt.legend()
             ax2.get_yaxis().set_major_locator(LinearLocator(numticks=5))
-            ax2.set_ylim([0, 100.0])
+            ax2.set_ylim([0, ylim])
             plt.grid()
             plt.ylabel("Longitudinal")
 
@@ -339,7 +341,7 @@ class BeamChar(AbstractTool):
                 plt.plot(plots["plot_data{}".format(n)]["energy"], plots["plot_data{}".format(n)]["zHalo"], lw=0.8,
                          label=plots["plot_data{}".format(n)]["name"])
             ax3.get_yaxis().set_major_locator(LinearLocator(numticks=5))
-            ax3.set_ylim([0, 100.0])
+            ax3.set_ylim([0, ylim])
             plt.grid()
             plt.xlabel("Energy (MeV/amu)")
             plt.ylabel("Vertical")
